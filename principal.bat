3583fc65d8e071d5e4f3624756f4708f9ad5771b
thomas@katalisindonesia.com <thomas@katalisindonesia.com>
2013-08-18 11:45:22 +0700

    modified prospermobile.url

--- principal.bat
+++ principal.bat
@@ -1,4 +1,4 @@
 git remote set-url origin git://github.com/thomedw/prosperhmt.git
 git reset --hard
 git pull
-start javaw -Xmx512m -jar principal.jar
+start javaw -Xmx512m -Dprospermobile.url=http://prospermobile.katalis.cloudbees.net/ -jar principal.jar
